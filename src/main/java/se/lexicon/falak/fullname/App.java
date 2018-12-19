0afa787ac4f129f64c1b24c3d8720acd701e20a6
falakbarakat <45755774+falakbarakat@users.noreply.github.com>
2018-12-19 15:52:30 +0100

    first one

---
+++ src/main/java/se/lexicon/falak/fullname/App.java
@@ -0,0 +1,17 @@
+package se.lexicon.falak.fullname;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+ //      creating an object in the class called WriteName
+    	WriteName obj = new WriteName();
+ //      calling method called myname while printing it   	
+        System.out.println(obj.myname());
+        
+    }
+}
