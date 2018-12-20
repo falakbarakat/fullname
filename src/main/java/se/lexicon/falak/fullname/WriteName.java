51429ab000d421c110d177c272e732804b9884f3
falakbarakat <45755774+falakbarakat@users.noreply.github.com>
2018-12-20 10:20:13 +0100

    test

---
+++ src/main/java/se/lexicon/falak/fullname/WriteName.java
@@ -0,0 +1,17 @@
+package se.lexicon.falak.fullname;
+// we create class calls WriteName
+public class  WriteName {
+//notice that the name of class must be the same name of WriteName.JAVA
+	 private String name2="barakat";
+	 private String name1="falak";
+//	we  use a method calls myname in the class WriteName;
+	public String myname() {
+		String myname=name1+" "+name2;
+		
+		return myname;
+//notice that we place the ending baranteth direct under the second word public		
+	}
+	
+//notice that we place the ending baranteth direct under the first word public	
+}
+
